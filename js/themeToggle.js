13e142b95c8874d35e32c1ff8e3a466f64bcb348
Joffre Moreira Pico <68706205+joramopi@users.noreply.github.com>
2025-06-06 21:54:05 -0500

    Move theme toggle next to emblem

--- js/themeToggle.js
+++ js/themeToggle.js
@@ -7,13 +7,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
   button.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-    const icon = button.querySelector('i');
-    if (document.body.classList.contains('dark-mode')) {
-      icon.classList.remove('fa-moon');
-      icon.classList.add('fa-sun');
-    } else {
-      icon.classList.remove('fa-sun');
-      icon.classList.add('fa-moon');
-    }
   });
 });
